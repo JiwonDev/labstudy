79546710e8455f7eb5ee79d2a9fa066756f5df5f
jiwondev <jiwondev@i-nara.co.kr>
2025-04-13 13:25:53 +0900

    feat: git init

---
+++ src/main/kotlin/com/labstudy/LabstudyApplication.kt
@@ -0,0 +1,11 @@
+package com.labstudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LabstudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<LabstudyApplication>(*args)
+}
---
+++ src/test/kotlin/com/labstudy/LabstudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.labstudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LabstudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
